790ef991eb5a9d7a87a07b958b9f98b4a97b743e
Facundo15 <48262116+Facundo15@users.noreply.github.com>
2023-07-31 00:27:17 -0300

    updated

--- js/init.js
+++ js/init.js
@@ -9,6 +9,43 @@ function openContact(event) {
     window.location.href = location;
 }
 
+
+function loadIframes() {
+    const iframesContaienrs = document.querySelectorAll('.iframe-container');
+
+    const loadLasyIframe = (iframeContaiener) => {
+        const iframeSrc = iframeContaiener.getAttribute("data-src");
+
+        if (iframeSrc) {
+            const iframe = document.createElement('iframe');
+            iframe.setAttribute('src', iframeSrc);
+            iframe.setAttribute('frameborder', '0');
+            iframe.height = "190px";
+            iframe.width = "500px"
+            iframeContaiener.appendChild(iframe);
+            iframeContaiener.removeAttribute('data-src');
+        }
+    };
+
+    const intersectObserver = new IntersectionObserver(enties => {
+        enties.forEach(entry => {
+            if (entry.isIntersecting) {
+                const target = entry.target;
+                loadLasyIframe(target);
+                intersectObserver.unobserve(target);
+            }
+        });
+    });
+
+    iframesContaienrs.forEach(iframeContainer => {
+        intersectObserver.observe(iframeContainer);
+    })
+}
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('contact-form').addEventListener('submit', openContact);
+
+    loadIframes();
 });
