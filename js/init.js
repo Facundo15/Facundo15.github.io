fc2981da4ed8fb543860fdd7b14086123cb01635
Facundo15 <48262116+Facundo15@users.noreply.github.com>
2023-07-31 02:32:55 -0300

    new changes

--- js/init.js
+++ js/init.js
@@ -1,6 +1,14 @@
 
 const emailContact = 'facufernandez.dev1820@gmail.com';
 
+new PerformanceObserver((list) => {
+    const latestEntry = list.getEntries().at(-1);
+
+    if (latestEntry?.element?.getAttribute('loading') == 'lazy') {
+        console.warn('Warning: LCP element was lazy loaded', latestEntry);
+    }
+}).observe({ type: 'largest-contentful-paint', buffered: true });
+
 function openContact(event) {
     event.preventDefault();
     const fullname = document.getElementById('input-fullname').value;
@@ -20,8 +28,8 @@ function loadIframes() {
             const iframe = document.createElement('iframe');
             iframe.setAttribute('src', iframeSrc);
             iframe.setAttribute('frameborder', '0');
-            iframe.height = "190px";
-            iframe.width = "500px"
+            iframe.height = "200px";
+            iframe.width = "450px"
             iframeContaiener.appendChild(iframe);
             iframeContaiener.removeAttribute('data-src');
         }
